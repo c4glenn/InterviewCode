8f3f97d831fa23e14225e3a9110562430c2dde2e
Philip Smith <pgsmith2@students.wcpss.net>
2019-04-06 21:25:48 -0400

    Allows you to select a file and displays it in picture box

---
+++ InterveiwCodeProjectTest1/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim opf As New OpenFileDialog
+
+        opf.Filter = "Choose Image(*.jpg;*.png;*.gif;*.jpeg)|*.jpg;*.png;*.gif;*.jpeg"
+
+        If opf.ShowDialog = DialogResult.OK Then
+            PictureBox1.Image = Image.FromFile(opf.FileName)
+        End If
+
+    End Sub
+End Class
