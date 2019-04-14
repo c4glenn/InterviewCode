7fdaae909acc4bf0d4f6e92cb9c36d62b44fe0a5
Philip Smith <pgsmith2@students.wcpss.net>
2019-04-14 15:08:23 -0400

    Succusfully does HSL conversion

--- InterveiwCodeProjectTest1/Form1.Designer.vb
+++ InterveiwCodeProjectTest1/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,10 +20,18 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Me.btnSelectImage = New System.Windows.Forms.Button()
         Me.picImage = New System.Windows.Forms.PictureBox()
+        Me.lblHue = New System.Windows.Forms.Label()
+        Me.lblSaturation = New System.Windows.Forms.Label()
+        Me.lblLightness = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.lblHPrompt = New System.Windows.Forms.Label()
+        Me.lblSPrompt = New System.Windows.Forms.Label()
+        Me.lblLPrompt = New System.Windows.Forms.Label()
         CType(Me.picImage, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -38,27 +46,107 @@ Partial Class Form1
         '
         'picImage
         '
-        Me.picImage.Location = New System.Drawing.Point(0, 56)
+        Me.picImage.Cursor = System.Windows.Forms.Cursors.Cross
+        Me.picImage.Enabled = False
+        Me.picImage.Location = New System.Drawing.Point(12, 41)
         Me.picImage.Name = "picImage"
-        Me.picImage.Size = New System.Drawing.Size(240, 171)
+        Me.picImage.Size = New System.Drawing.Size(528, 397)
         Me.picImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
         Me.picImage.TabIndex = 1
         Me.picImage.TabStop = False
+        '
+        'lblHue
+        '
+        Me.lblHue.AutoSize = True
+        Me.lblHue.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHue.Location = New System.Drawing.Point(205, 13)
+        Me.lblHue.Name = "lblHue"
+        Me.lblHue.Size = New System.Drawing.Size(24, 25)
+        Me.lblHue.TabIndex = 2
+        Me.lblHue.Text = "0"
+        '
+        'lblSaturation
+        '
+        Me.lblSaturation.AutoSize = True
+        Me.lblSaturation.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSaturation.Location = New System.Drawing.Point(346, 12)
+        Me.lblSaturation.Name = "lblSaturation"
+        Me.lblSaturation.Size = New System.Drawing.Size(24, 25)
+        Me.lblSaturation.TabIndex = 3
+        Me.lblSaturation.Text = "0"
+        '
+        'lblLightness
+        '
+        Me.lblLightness.AutoSize = True
+        Me.lblLightness.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLightness.Location = New System.Drawing.Point(485, 12)
+        Me.lblLightness.Name = "lblLightness"
+        Me.lblLightness.Size = New System.Drawing.Size(24, 25)
+        Me.lblLightness.TabIndex = 4
+        Me.lblLightness.Text = "0"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        '
+        'lblHPrompt
+        '
+        Me.lblHPrompt.AutoSize = True
+        Me.lblHPrompt.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHPrompt.Location = New System.Drawing.Point(166, 12)
+        Me.lblHPrompt.Name = "lblHPrompt"
+        Me.lblHPrompt.Size = New System.Drawing.Size(33, 25)
+        Me.lblHPrompt.TabIndex = 5
+        Me.lblHPrompt.Text = "H:"
+        '
+        'lblSPrompt
+        '
+        Me.lblSPrompt.AutoSize = True
+        Me.lblSPrompt.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSPrompt.Location = New System.Drawing.Point(308, 12)
+        Me.lblSPrompt.Name = "lblSPrompt"
+        Me.lblSPrompt.Size = New System.Drawing.Size(32, 25)
+        Me.lblSPrompt.TabIndex = 6
+        Me.lblSPrompt.Text = "S:"
+        '
+        'lblLPrompt
+        '
+        Me.lblLPrompt.AutoSize = True
+        Me.lblLPrompt.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLPrompt.Location = New System.Drawing.Point(447, 11)
+        Me.lblLPrompt.Name = "lblLPrompt"
+        Me.lblLPrompt.Size = New System.Drawing.Size(30, 25)
+        Me.lblLPrompt.TabIndex = 7
+        Me.lblLPrompt.Text = "L:"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.ClientSize = New System.Drawing.Size(552, 450)
+        Me.Controls.Add(Me.lblLPrompt)
+        Me.Controls.Add(Me.lblSPrompt)
+        Me.Controls.Add(Me.lblHPrompt)
+        Me.Controls.Add(Me.lblLightness)
+        Me.Controls.Add(Me.lblSaturation)
+        Me.Controls.Add(Me.lblHue)
         Me.Controls.Add(Me.picImage)
         Me.Controls.Add(Me.btnSelectImage)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "HSL Value Finder"
         CType(Me.picImage, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents btnSelectImage As Button
     Friend WithEvents picImage As PictureBox
+    Friend WithEvents lblHue As Label
+    Friend WithEvents lblSaturation As Label
+    Friend WithEvents lblLightness As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents lblHPrompt As Label
+    Friend WithEvents lblSPrompt As Label
+    Friend WithEvents lblLPrompt As Label
 End Class
--- InterveiwCodeProjectTest1/Form1.vb
+++ InterveiwCodeProjectTest1/Form1.vb
@@ -1,17 +1,95 @@
 ﻿Public Class Form1
+
+
+    Dim bm As Bitmap
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnSelectImage.Click
         Dim opf As New OpenFileDialog
 
-        opf.Filter = "Choose Image(*.jpg;*.png;*.gif;*.jpeg)|*.jpg;*.png;*.gif;*.jpeg"
+        opf.Filter = "Choose Image(*.jpg;*.png;*.gif;*.jpeg;*.Bmp)|*.jpg;*.png;*.gif;*.jpeg;*.Bmp"
+
 
         If opf.ShowDialog = DialogResult.OK Then
+
             Try
-                picImage.Image = Image.FromFile(opf.FileName)
+                Dim importedImage As Image = Image.FromFile(opf.FileName)
+                bm = New Bitmap(importedImage)
+                picImage.Image = bm
+                picImage.Enabled = True
+
             Catch ex As Exception
                 MessageBox.Show("Something is wrong with the file you selected")
             End Try
 
         End If
+    End Sub
+    Private Sub picImage_MouseClick(sender As Object, e As MouseEventArgs) Handles picImage.MouseClick
+
+        getHSV(e)
+
+
 
     End Sub
+
+    Sub getHS(ByRef e As MouseEventArgs)
+        Try
+            Dim c As Color = bm.GetPixel(e.X, e.Y)
+            lblHue.Text = (c.GetHue).ToString("##.00")
+            lblSaturation.Text = c.GetSaturation.ToString("0.000")
+            'lblValue.Text = c.
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    'code based off algorithm on https://www.rapidtables.com/convert/color/rgb-to-hsl.html
+    Sub getHSV(ByRef e As MouseEventArgs)
+        Dim max As Double
+        Dim min As Double
+        Dim diff As Double
+
+        Dim c As Color = bm.GetPixel(e.X, e.Y)
+
+        Dim R As Double = c.R / 255
+        Dim G As Double = c.G / 255
+        Dim B As Double = c.B / 255
+
+        Dim H As Double
+        Dim S As Double
+        Dim L As Double
+
+
+        ' Get the maximum and minimum RGB components.
+        max = R
+        If max < G Then max = G
+        If max < B Then max = B
+
+        min = R
+        If min > G Then min = G
+        If min > B Then min = B
+
+        diff = max - min
+        L = (max + min) / 2
+        If diff < 0.00001 Then
+            S = 0
+            H = 0
+        Else
+            S = diff / (1 - Math.Abs((2 * L) - 1))
+            If max = R Then
+                H = 60 * (((G - B) / diff) Mod 6)
+            ElseIf max = G Then
+                H = 60 * (((B - R) / diff) + 2)
+            Else
+                 H = 60 * ((R - R) / diff) + 4)
+            End If
+
+        End If
+
+        lblHue.Text = H.ToString("##.00")
+        lblSaturation.Text = S.ToString("###.##%")
+        lblLightness.Text = L.ToString("###.##%")
+
+
+    End Sub
+
+
+
 End Class
