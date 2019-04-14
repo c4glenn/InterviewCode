6952e24a1f2bb03ba1ce54b31385abaecd20fee4
Philip Smith <pgsmith2@students.wcpss.net>
2019-04-14 16:16:16 -0400

    removed an accidental resource

--- InterveiwCodeProjectTest1/My Project/Resources.Designer.vb
+++ InterveiwCodeProjectTest1/My Project/Resources.Designer.vb
@@ -59,15 +59,5 @@ Namespace My.Resources
                 resourceCulture = value
             End Set
         End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
-        Friend ReadOnly Property _1200px_SNice() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("1200px-SNice", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
     End Module
 End Namespace
